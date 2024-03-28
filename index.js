045b1b374fcd4b4c8f5e7229d9ab25e1aa629ceb
clenda <glendabittok24@gmail.com>
2024-03-28 12:37:38 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+
+function superbowlWin(record) {
+    // Iterate over each entry in the record array
+    for (let i = 0; i < record.length; i++) {
+      // Check if the result is a win ('W')
+      if (record[i].result === 'W') {
+        // If it is, return the corresponding year
+        return record[i].year;
+      }
+    }
+    // If no win is found, return undefined
+    return undefined;
+  }
+  
+
+
+
+
